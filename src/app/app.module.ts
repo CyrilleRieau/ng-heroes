8bc4822000b59662f9f125375869c873f02c46af
Cyrille <cyrille.rieau@gmail.com>
2017-10-03 17:24:04 +0200

    Test Heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http' 
 import { AppComponent } from './app.component';
+import { ListHeroesComponent } from './list-heroes/list-heroes.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroesService } from './heroes.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListHeroesComponent,
+    HeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
